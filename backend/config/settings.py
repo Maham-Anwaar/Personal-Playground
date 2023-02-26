95a79e920b1fb606b43e2c5a7445c8393328ac3a
Maham <mhm.anwaar2@gmail.com>
2023-02-26 06:21:21 +0500

    feat: added postgresql as database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,11 +95,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
