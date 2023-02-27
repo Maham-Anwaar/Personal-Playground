c3bcc4dd3c44c503141df2c3b78d05dc64064d92
Maham <mhm.anwaar2@gmail.com>
2023-02-27 07:23:44 +0500

    feat: added front end for reminder screen

--- frontend/src/pages/dashboard/GeneralApp.js
+++ frontend/src/pages/dashboard/GeneralApp.js
@@ -6,17 +6,20 @@ import { PATH_DASHBOARD } from '../../routes/paths'
 // components
 import Page from '../../components/Page'
 import HeaderBreadcrumbs from '../../components/HeaderBreadcrumbs'
+import CalendarMain from './CalendarMain'
 
 export default function GeneralApp() {
   const { themeStretch } = useSettings()
 
   const isCalendarPage = window.location.pathname.includes('calendar');
+  const isMainPage = window.location.pathname.includes('app');
+  
   return (
     <Page title="App: Frontend">
       <Container maxWidth={themeStretch ? false : 'lg'}>
-        <HeaderBreadcrumbs heading="Welcome" links={[{ name: 'Home', href: PATH_DASHBOARD.root }]} />
+        {isMainPage && <HeaderBreadcrumbs heading="Welcome" links={[{ name: 'Home', href: PATH_DASHBOARD.root }]} />}
+        {isCalendarPage && <CalendarMain/>}
       </Container>
-      {isCalendarPage && <div className={"bg-green-500 text-white text-center py-2 px-4 rounded-md"}>Calendar is here!</div>}
     </Page>
   )
 }
